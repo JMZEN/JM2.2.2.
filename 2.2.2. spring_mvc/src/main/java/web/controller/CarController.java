6b275722b691e665c836c03b68b0153a319f3d83
JMZEN <lamp507@yandex.ru>
2020-09-17 23:34:11 +0300

    4/5 done

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import web.models.Car;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,12 +14,23 @@ import java.util.List;
 @RequestMapping("/cars")
 public class CarController {
 
+    private List<Car> cars;
+
+    @PostConstruct
+    private void localData() {
+        cars = new ArrayList<>();
+
+        Car car1 = new Car(1L, "BMW", "X5");
+        Car car2 = new Car(2L, "Renault", "Logan");
+        Car car3 = new Car(3L, "Audi", "A5");
+
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+    }
+
     @GetMapping("/table")
     public String carTable(Model model) {
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car(1L, "BMW", "X5"));
-        cars.add(new Car(2L, "Renault", "Logan"));
-        cars.add(new Car(3L, "Audi", "A5"));
         model.addAttribute("cars", cars);
         return "cars-table";
     }
